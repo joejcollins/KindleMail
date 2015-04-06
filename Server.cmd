d82837237b02d50e342161d0851b56632321f06e
Joe Collins <joejcollins@gmail.com>
2015-04-07 00:03:31 +0100

    Changed arbitary text.

--- Server.cmd
+++
@@ -1,4 +0,0 @@
-:: Run up the GAE development server
-c:\python27\python "c:\Program Files\Google\google_appengine\dev_appserver.py" ./ --host=0.0.0.0
-pause
- 
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello Worms'
+    return 'Hello Worm'
 
 
 @app.errorhandler(404)
