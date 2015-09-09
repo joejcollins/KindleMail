dcfa9867806f7245fed71203ca6c15f716d73cd3
Joe Collins <joejcollins@gmail.com>
2015-09-09 15:33:51 +0100

    Check port access from Azure

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using MailKit;
 using MailKit.Net.Imap;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
@@ -12,33 +13,37 @@ namespace WebApp.Controllers
     {
         public ActionResult Index()
         {
-            //using (var client = new ImapClient())
-            //{
-            //    client.Connect("imap.friends.com", 993, true);
+            var viewModel = new MessageListSummary();
+            using (var client = new ImapClient())
+            {
+                client.Connect("imap.gmail.com", 993, true);
 
-            //    // Note: since we don't have an OAuth2 token, disable
-            //    // the XOAUTH2 authentication mechanism.
-            //    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                // Note: since we don't have an OAuth2 token, disable
+                // the XOAUTH2 authentication mechanism.
+                // client.AuthenticationMechanisms.Remove("XOAUTH2");
+                var password = Environment.GetEnvironmentVariable("PASSWORD");
 
-            //    client.Authenticate("joejcollins@gmail.com", "password");
+                client.Authenticate("joejcollins@gmail.com", password);
 
-            //    // The Inbox folder is always available on all IMAP servers...
-            //    var inbox = client.Inbox;
-            //    inbox.Open(FolderAccess.ReadOnly);
+                // The Inbox folder is always available on all IMAP servers...
+                var inbox = client.Inbox;
+                inbox.Open(FolderAccess.ReadOnly);
 
-            //    Console.WriteLine("Total messages: {0}", inbox.Count);
-            //    Console.WriteLine("Recent messages: {0}", inbox.Recent);
+                viewModel.Total = inbox.Count;
 
-            //    for (int i = 0; i < inbox.Count; i++)
-            //    {
-            //        var message = inbox.GetMessage(i);
-            //        Console.WriteLine("Subject: {0}", message.Subject);
-            //    }
+                //Console.WriteLine("Total messages: {0}", inbox.Count);
+                //Console.WriteLine("Recent messages: {0}", inbox.Recent);
 
-            //    client.Disconnect(true);
-            //}
+                //for (int i = 0; i < inbox.Count; i++)
+                //{
+                //    var message = inbox.GetMessage(i);
+                //    Console.WriteLine("Subject: {0}", message.Subject);
+                //}
 
-            return View();
+                client.Disconnect(true);
+            }
+
+            return View(viewModel);
         }
 
         public ActionResult About()
---
+++ WebApp/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class Message
+    {
+        public string Title { get; set; }
+        public string From { get; set; }
+    }
+}
