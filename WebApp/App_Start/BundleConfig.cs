282dd39eb0031e17f2cb69ec3623c2381db3880a
joeblackradley <joe_collins@blackradley.com>
2015-09-12 08:16:08 +0100

    Part way through keypress on enter for Kindle

--- WebApp/App_Start/BundleConfig.cs
+++ WebApp/App_Start/BundleConfig.cs
@@ -24,7 +24,6 @@ namespace WebApp
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
